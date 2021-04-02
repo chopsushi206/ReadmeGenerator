485069296beb1442f0b0c07789d920bd57580548
LiHua Anderson <lihua.anderson@gmail.com>
2021-04-01 23:12:14 -0700

    added variables for requirements and questions to obtain data about project, and added a promise to console log data afterwards

--- index.js
+++ index.js
@@ -1,13 +1,58 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the project name?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of the project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please include any necessary installation instructions for the project.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please include any usage information for the project.',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Please include any contribution instructions for the project.',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please include any test instructions for the project.',
+    },
+    {
+        type: 'input',
+        name: 'userName',
+        message: 'What is your github username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail address?'
+    }
+])};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then((data) => console.log(data))
+ }
 
 // Function call to initialize app
 init();
